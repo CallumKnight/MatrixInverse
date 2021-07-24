059e3ae7a06c837c3a2881dee9a949acae87b6c0
Callum Knight <calknight@hotmail.com>
2021-07-24 13:25:07 +0100

    Updated README.md, and comments within code.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # MatrixInverse
-Calculates the inverse of a given matrix.
+Calculates the inverse of a given matrix of any size.
+
+This program is designed using vector notation.
+
+It has developed with simplicity and readability in mind.
+
+The algorithm contains two main components: 
+1. Inverse function.
+2. Determinant function. 
+
+The inverse function carries out the following calculation: 
+
+Inverse = (1/Determinant) * CofactorTranspose
+
+It calculates the Determinant and CofactorTranspose using the Sign Matrix and the Matrix of Minors. 
+
+The determinant function is recursive, enabling this program to calculate the inverse of any sized matrix.
+
+The program outputs all steps of the process on the terminal displaying:
+1. Matrix for which inverse must be obtained.
+2. Sign matrix.
+3. Minor matrix.
+4. Cofactor matrix.
+5. Cofactor matrix transposed.
+6. Determinant.
+7. Inverse.
+
+If an inverse does not exist for the given matrix, a message indicating this will be displayed on the terminal when run.
+
+
--- main.cpp
+++ main.cpp
@@ -11,19 +11,11 @@ void printMatrix(std::vector<std::vector<double>> matrix);
 
 // Main
 int main(){
-    
-    std::vector<std::vector<double>> testMatrix = {{1,2,1,3,4},{2,1,2,5,8},{1,2,2,1,1},{9,9,9,9,9},{7,6,1,1,2}};
-    //std::vector<std::vector<double>> testMatrix = {{1,2,1},{2,1,2},{1,2,2}};
-    //std::vector<std::vector<double>> testMatrix = {{1,2,3},{4,5,6},{7,8,9}}; // No inverse
-    //std::vector<std::vector<double>> testMatrix = {{1,2},{3,4}};
-    //std::vector<std::vector<double>> testMatrix = {{1,1},{2,2}}; // No inverse
-    //std::vector<std::vector<double>> testMatrix = {{2}};
-    //std::vector<std::vector<double>> testMatrix = {{0}}; // No inverse
-    std::vector<std::vector<double>> testInverse;
+    // Enter matrix to be inverted here (outer vector contains rows, inner vector contains column elements)
+    std::vector<std::vector<double>> matrix = {{-1,-1,2},{2,1,1},{3,4,5}};
+    std::vector<std::vector<double>> inverse;
 
-    testInverse = calculateInverseOfMatrix(testMatrix);
-
-    printMatrix(testInverse);
+    inverse = calculateInverseOfMatrix(matrix);
 
     return 0;
 }
@@ -39,150 +31,166 @@ std::vector<std::vector<double>> calculateInverseOfMatrix(std::vector<std::vecto
     std::vector<std::vector<double>> cofactorMatrixTranspose;
     double determinant = 0;
 
+    std::cout << "Calculating inverse of:" << std::endl;
+    printMatrix(matrix);
+    
+    if(matrix[0].size() == 1){ // Matrix dimension equals 1, inverse is simply the reciprocal of the matrix value
 
-    if(matrix[0].size() == 1){
-        
-        std::vector<double> inverseRow;
+        if(matrix[0][0] != 0){ // Matrix value is not equal to 0, reciprocal exists, therefore inverse exists
 
-        if(matrix[0][0] != 0){
+            std::vector<double> inverseRow;
 
             inverseRow.push_back(1 / matrix[0][0]);
             inverseMatrix.push_back(inverseRow);
 
-            std::cout << "Inverse calculated." << std::endl;
+            std::cout << "Inverse:" << std::endl;
+            printMatrix(inverseMatrix);
         }
-        else{
+        else{ // Matrix value equals 0, reciprocal value does not exist, therefore inverse does not exist
 
-            inverseRow.push_back(0);
-            inverseMatrix.push_back(inverseRow);
-
-            std::cout << "Inverse does not exist for this matrix." << std::endl;
-        }
-        
-        
-    }
-    else{
-        
-        // Construct sign matrix
-        for(int i = 1; i < matrix[0].size()+1; i++){
-            
-            std::vector<double> signRow;
-            
-            // Populate the row
-            for(int j = 1; j < matrix[0].size()+1; j++){ 
-                signRow.push_back(pow(-1, i+j));
-            }
-
-            // Add row to matrix
-            signMatrix.push_back(signRow);    
-        }
-
-        std::cout << "Sign matrix constructed." << std::endl;
-        printMatrix(signMatrix);
-        
-        // Construct minorMatrix
-        for(int i = 0; i < matrix[0].size(); i++){
-
-            std::vector<double> minorRow;
-            
-            for(int j = 0; j < matrix[0].size(); j++){
-                
-                std::vector<std::vector<double>> reducedMatrix;
-
-                // Construct reduced matrix with the row and column of minor being calculated removed
-                for(int k = 0; k < matrix[0].size(); k++){
-
-                    std::vector<double> reducedRow;
-
-                    for(int l = 0; l < matrix[0].size(); l++){
-                        
-                        // Copy elements that are not equal to the column of the minor being calculated
-                        if(l != j){
-                            
-                            reducedRow.push_back(matrix[k][l]);
-                        }
-                    }
-
-                    // Add row to temporary minor matrix if it does not equal to the row of the minor being calculated  
-                    if(k != i){
-                        
-                        reducedMatrix.push_back(reducedRow);
-                    }
-                }
-                
-                minorRow.push_back(calculateDeterminant(reducedMatrix));
-            }
-
-            minorMatrix.push_back(minorRow); 
-        }
-
-        std::cout << "Minor matrix constructed." << std::endl;
-        printMatrix(minorMatrix);
-
-        // Construct cofactor matrix
-        for(int i = 0; i < matrix[0].size(); i++){
-            
-            std::vector<double> cofactorRow;
-
-            for(int j = 0; j < matrix[0].size(); j++){
-                cofactorRow.push_back(signMatrix[i][j]*minorMatrix[i][j]);
-            }
-
-            cofactorMatrix.push_back(cofactorRow);
-        }
-
-        std::cout << "Cofactor matrix constructed." << std::endl;
-        printMatrix(cofactorMatrix);
-
-        // Transpose cofactor matrix
-        for(int i = 0; i < matrix[0].size(); i++){
-
-            std::vector<double> transposeRow;
-            
-            for(int j = 0; j < matrix[0].size(); j++){
-                transposeRow.push_back(cofactorMatrix[j][i]);
-            }
-
-            cofactorMatrixTranspose.push_back(transposeRow);
-        }
-
-        std::cout << "Cofactor matrix transposed." << std::endl;
-        printMatrix(cofactorMatrixTranspose);
-
-        // Calculate determinant
-        for(int i = 0; i < matrix[0].size(); i++){
-            
-            determinant += (signMatrix[0][i]*minorMatrix[0][i]*matrix[0][i]);
-        }
-
-        std::cout << "Determinant calculated." << std::endl;
-        std::cout << determinant << std::endl;
-
-        // Calculate inverse matrix
-        if(determinant == 0){
-            
             std::vector<double> inverseRow;
 
             inverseRow.push_back(0);
             inverseMatrix.push_back(inverseRow);
 
-            std::cout << "Inverse does not exist for this matrix." << std::endl;
-        }
-        else{
+            std::cout << "Inverse does not exist for this matrix, cannot divide by zero." << std::endl;
+        }  
+    }
+    else{ // Matrix dimension is > 1, inverse algorithm is used
 
-            for(int i = 0; i < matrix[0].size(); i++){
+        if(matrix.size() != matrix[0].size()){ // Matrix is not NxN dimensional, therefore inverse does not exist
+
+            std::vector<double> inverseRow;
+
+            inverseRow.push_back(0);
+            inverseMatrix.push_back(inverseRow);
+
+            std::cout << "Inverse does not exist for this matrix, matrix must be NxN dimensional." << std::endl;
+        }
+        else{ // Matrix is NxN dimensional
+
+            // Construct sign matrix
+            for(int i = 1; i < matrix[0].size()+1; i++){
                 
-                std::vector<double> inverseRow;
+                std::vector<double> signRow;
                 
-                for(int j = 0; j < matrix[0].size(); j++){
-                    inverseRow.push_back(cofactorMatrixTranspose[i][j] / determinant);
+                for(int j = 1; j < matrix[0].size()+1; j++){ 
+                    signRow.push_back(pow(-1, i+j));
                 }
 
-                inverseMatrix.push_back(inverseRow);
+                signMatrix.push_back(signRow);    
             }
 
-            std::cout << "Inverse calculated." << std::endl;
-        }        
+            std::cout << "Sign matrix:" << std::endl;
+            printMatrix(signMatrix);
+            
+            // Construct minorMatrix
+            for(int i = 0; i < matrix[0].size(); i++){
+
+                std::vector<double> minorRow;
+                
+                for(int j = 0; j < matrix[0].size(); j++){
+                    
+                    std::vector<std::vector<double>> reducedMatrix;
+
+                    // Construct reduced matrix with the row and column of minor being calculated removed
+                    for(int k = 0; k < matrix[0].size(); k++){
+
+                        std::vector<double> reducedRow;
+
+                        for(int l = 0; l < matrix[0].size(); l++){
+                            
+                            // Copy elements that are not equal to the column of the minor being calculated
+                            if(l != j){
+                                
+                                reducedRow.push_back(matrix[k][l]);
+                            }
+                        }
+
+                        // Add row to reduced matrix if it does not equal to the row of the minor being calculated  
+                        if(k != i){
+                            
+                            reducedMatrix.push_back(reducedRow);
+                        }
+                    }
+                    
+                    // Calculate determinant of reduced matrix, append result to the row of minors
+                    minorRow.push_back(calculateDeterminant(reducedMatrix));
+                }
+
+                // Append row of minors to the matrix of minors
+                minorMatrix.push_back(minorRow); 
+            }
+
+            std::cout << "Minor matrix:" << std::endl;
+            printMatrix(minorMatrix);
+
+            // Construct cofactor matrix
+            for(int i = 0; i < matrix[0].size(); i++){
+                
+                std::vector<double> cofactorRow;
+
+                for(int j = 0; j < matrix[0].size(); j++){
+                    cofactorRow.push_back(signMatrix[i][j]*minorMatrix[i][j]);
+                }
+
+                cofactorMatrix.push_back(cofactorRow);
+            }
+
+            std::cout << "Cofactor matrix:" << std::endl;
+            printMatrix(cofactorMatrix);
+
+            // Transpose cofactor matrix
+            for(int i = 0; i < matrix[0].size(); i++){
+
+                std::vector<double> transposeRow;
+                
+                for(int j = 0; j < matrix[0].size(); j++){
+                    transposeRow.push_back(cofactorMatrix[j][i]);
+                }
+
+                cofactorMatrixTranspose.push_back(transposeRow);
+            }
+
+            std::cout << "Cofactor matrix transposed:" << std::endl;
+            printMatrix(cofactorMatrixTranspose);
+
+            // Calculate determinant
+            for(int i = 0; i < matrix[0].size(); i++){
+                
+                determinant += (signMatrix[0][i]*minorMatrix[0][i]*matrix[0][i]);
+            }
+
+            std::cout << "Determinant:" << std::endl;
+            std::cout << determinant << std::endl;
+
+            // Calculate inverse matrix
+            if(determinant == 0){ // Determinant equals 0, cannot divide by 0, therefore inverse does not exist 
+                
+                std::vector<double> inverseRow;
+
+                inverseRow.push_back(0);
+                inverseMatrix.push_back(inverseRow);
+
+                std::cout << "Inverse does not exist for this matrix, determinant is equal to zero." << std::endl;
+            }
+            else{ // Determinant is not equal to 0, therefore inverse does exist
+
+                for(int i = 0; i < matrix[0].size(); i++){
+                    
+                    std::vector<double> inverseRow;
+                    
+                    for(int j = 0; j < matrix[0].size(); j++){
+                        inverseRow.push_back(cofactorMatrixTranspose[i][j] / determinant);
+                    }
+
+                    inverseMatrix.push_back(inverseRow);
+                }
+
+                std::cout << "Inverse:" << std::endl;
+                printMatrix(inverseMatrix);
+            }   
+        }     
         
     }
 
@@ -196,13 +204,12 @@ double calculateDeterminant(std::vector<std::vector<double>> matrix) {
     std::vector<double> minorRow;
     double determinant = 0;
     
-    // If matrix dimension == 1, return the value within the matrix
-    if(matrix[0].size() == 1){ 
+    
+    if(matrix[0].size() == 1){ // Matrix dimension equals 1, return the value within the matrix
         
         determinant = matrix[0][0];
     }
-    // Else calculate determinant using standard approach
-    else{ 
+    else{ // Calculate determinant using standard approach
 
         // Populate the row of signs
         for(int i = 0; i < matrix[0].size(); i++){ 
@@ -229,10 +236,11 @@ double calculateDeterminant(std::vector<std::vector<double>> matrix) {
                     }
                 }
 
-                // Add row to temporary minor matrix  
+                // Add row to reduced matrix  
                 reducedMatrix.push_back(reducedRow);
             }
             
+            // Calculate determinant of reduced matrix, append result to the row of minors
             minorRow.push_back(calculateDeterminant(reducedMatrix));
         } 
 
@@ -249,11 +257,11 @@ double calculateDeterminant(std::vector<std::vector<double>> matrix) {
 
 void printMatrix(std::vector<std::vector<double>> matrix){
 
-    for(int i = 0; i < matrix[0].size(); i++){
+    for(int i = 0; i < matrix.size(); i++){
         
         for(int j = 0; j < matrix[0].size(); j++){
             
-            std::cout << matrix[i][j] << "\t";
+            std::cout << matrix[i][j] << "  ";
         }
 
         std::cout << std::endl;
